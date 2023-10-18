81d156cc62d28ee03ba41eeb7de788fd62ad3f84
Laura López <lauralopez166@gmail.com>
2023-10-18 12:03:01 +0200

    añadido comentarios

---
+++ dwes1_2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello world</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Hello World"
+        ?>
+    </h1>
+    <p>
+        <?php
+            # Esto es un comentario
+            // Una linea de comentario
+            /*Esto es un comentario de
+            varias lineas*/
+            $saludo = "Bienvenido";
+            $saludo = "Bienvenido";
+            $nombre = "Pepe";
+            echo $saludo.", ".$nombre.".";
+        ?>
+    </p>
+</body>
+</html>
