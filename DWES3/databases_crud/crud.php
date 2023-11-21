720b1ba3434ea3c46b0fa1d27578cc2505596db2
Laura López <99351746+laurity@users.noreply.github.com>
2023-11-21 12:10:25 +0100

    añadir,modificar, borrar crud

--- DWES3/databases_crud/crud.php
+++ DWES3/databases_crud/crud.php
@@ -47,4 +47,37 @@
     echo "<h1>Pizzas tras añadir nueva pizza</h1>";
     listarPizzas($conn);
     
+    //Pizza modificada
+    $nombrePizza = "Pizza modificada";
+    $costePizza = 6.00;
+    $precioPizza = 11.00;
+    $ingredientesPizza = "Tomate, Mozzarela, Albahaca, Champiñones";
+    $nombreOriginal = "Pizza prueba";
+
+    $modificar = $conn->prepare("UPDATE pizzas SET nombre = :nombre, coste = :coste, precio = :precio,
+    ingredientes = :ingredientes WHERE nombre = :nombreOriginal");
+
+$modificar -> bindParam(':nombre', $nombrePizza);
+$modificar -> bindParam(':coste', $costePizza);
+$modificar -> bindParam(':precio', $precioPizza);
+$modificar -> bindParam(':ingredientes', $ingredientesPizza);
+$modificar -> bindParam(':nombreOriginal', $nombreOriginal);
+
+$modificar->execute();
+echo "<h1>Pizzas tras modificar</h1>";
+listarPizzas($conn);
+
+//Borrar Pizza
+
+$nombreABorrar = "Pizza modificada";
+
+$eliminar = $conn->prepare("DELETE FROM pizzas WHERE nombre = :nombre");
+
+$eliminar->bindParam(":nombre", $nombreABorrar);
+
+$eliminar->execute();
+echo "<h1>Pizzas tras borrado</h1>";
+listarPizzas($conn);
+
+
 ?>
