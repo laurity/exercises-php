d99b426a3bc291961b4c4bf4ed49cfea5002a1fe
Laura López <lauralopez166@gmail.com>
2023-11-28 16:20:30 +0100

    editar funcional

--- DWES_tar3_Lopez_Alonso_Laura/zona_admin.php
+++ DWES_tar3_Lopez_Alonso_Laura/zona_admin.php
@@ -34,12 +34,10 @@ function listarPizzas($conn)
         echo "<td>$row[nombre]</td><td>$row[ingredientes]</td><td>$row[precio]€</td><td>$row[coste]€</td>";
         echo "<td>";
 
-
         echo "<form action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "' method='post' style='display: inline;'>";
         echo "<input type='hidden' name='id' value='$row[id]'>";
         echo "<button type='submit' name='editar'>Editar</button>";
         echo "</form>";
-
 
         echo "<form action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "' method='post' style='display: inline;'>";
         echo "<input type='hidden' name='id' value='$row[id]'>";
@@ -52,14 +50,6 @@ function listarPizzas($conn)
 
     echo "</table>";
 }
-
-if (isset($_GET['logout'])) {
-    $_SESSION = array();
-    session_destroy();
-    header("Location: index.php");
-    exit();
-}
-
 
 function crearPizza($conn)
 {
@@ -95,13 +85,78 @@ function borrarPizza($conn, $idPizza)
     $eliminar->execute();
 }
 
+//Funciones del boton editar
+function obtenerPizzaPorId($conn, $idPizza)
+{
+    $consulta = $conn->prepare("SELECT id, nombre, ingredientes, precio, coste FROM pizzas WHERE id = :id");
+    $consulta->bindParam(':id', $idPizza);
+    $consulta->execute();
+    return $consulta->fetch(PDO::FETCH_ASSOC);
+}
+
+function editarPizza($conn)
+{
+    if (isset($_POST["id"])) {
+        $idPizza = $_POST["id"];
+        $pizza = obtenerPizzaPorId($conn, $idPizza);
+
+        if ($pizza) {
+            echo "<h2>Editar Pizza</h2>";
+            echo "<form action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "' method='post'>";
+            echo "<input type='hidden' name='id' value='$pizza[id]'>";
+            echo "<label for='nombrePizza'>Nombre de la Pizza:</label>";
+            echo "<input value='" . htmlspecialchars($pizza['nombre']) . "' name='nombrePizza' placeholder='Nombre de la Pizza...' required><br>";
+
+            echo "<label for='costePizza'>Coste de la Pizza:</label>";
+            echo "<input value='" . htmlspecialchars($pizza['coste']) . "' name='costePizza' placeholder='Coste de la Pizza...' required><br>";
+
+            echo "<label for='precioPizza'>Precio de la Pizza:</label>";
+            echo "<input value='" . htmlspecialchars($pizza['precio']) . "' name='precioPizza' placeholder='Precio de la Pizza...' required><br>";
+
+            echo "<label for='ingredientesPizza'>Ingredientes de la Pizza:</label>";
+            echo "<input value='" . htmlspecialchars($pizza['ingredientes']) . "' name='ingredientesPizza' placeholder='Ingredientes de la Pizza...' required><br>";
+
+            echo "<button type='submit' name='guardarEdicion'>Guardar Edición</button>";
+            echo "</form>";
+        }
+    }
+}
+
+function guardarEdicionPizza($conn)
+{
+    $idPizza = $_POST["id"];
+    $nombrePizza = $_POST["nombrePizza"];
+    $costePizza = floatval($_POST["costePizza"]);
+    $precioPizza = floatval($_POST["precioPizza"]);
+    $ingredientesPizza = $_POST["ingredientesPizza"];
+
+    $editar = $conn->prepare("UPDATE pizzas SET nombre = :nombre, coste = :coste, precio = :precio, ingredientes = :ingredientes WHERE id = :id");
+
+    $editar->bindParam(':id', $idPizza);
+    $editar->bindParam(':nombre', $nombrePizza);
+    $editar->bindParam(':coste', $costePizza);
+    $editar->bindParam(':precio', $precioPizza);
+    $editar->bindParam(':ingredientes', $ingredientesPizza);
+
+    try {
+        $editar->execute();
+    } catch (PDOException $e) {
+        echo "Error al editar la pizza: " . $e->getMessage();
+    }
+
+    header("Location: " . $_SERVER['PHP_SELF']);
+    exit;
+}
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["crear"])) {
         crearPizza($conn);
-    } elseif (isset($_POST["editar"])) {
-        //editarPizza($conn);
+    } elseif (isset($_POST["editar"])) {    //Para que se muestre el formulario
+        editarPizza($conn);
+    } elseif (isset($_POST["guardarEdicion"])) {    //Para que edite la pizza
+        guardarEdicionPizza($conn);
     } elseif (isset($_POST["borrar"])) {
-
         $idPizzaABorrar = $_POST["id"];
         borrarPizza($conn, $idPizzaABorrar);
     }
@@ -113,51 +168,43 @@ $nombrePizza = $costePizza = $precioPizza = $ingredientesPizza = '';
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style-admin.css">
-    <title>Administrador
-        <?php echo $_SESSION['nombre'] ?>
-    </title>
+    <title>Administrador<?php echo $_SESSION['nombre'] ?></title>
 </head>
 
 <body>
     <div class="container">
-        <div class="wrapper">
             <h1>Bienvenido, Admin</h1>
             <h1>Listado de Pizzas</h1>
             <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
                 <label for="nombrePizza">Nombre de la Pizza:</label>
-                <input value="<?php echo htmlspecialchars($nombrePizza); ?>" name="nombrePizza"
-                    placeholder="Nombre de la Pizza..." required><br>
+                <input value="<?php echo htmlspecialchars($nombrePizza); ?>" name="nombrePizza" placeholder="Nombre de la Pizza..." required><br>
 
                 <label for="costePizza">Coste de la Pizza:</label>
-                <input value="<?php echo htmlspecialchars($costePizza); ?>" name="costePizza"
-                    placeholder="Coste de la Pizza..." required><br>
+                <input value="<?php echo htmlspecialchars($costePizza); ?>" name="costePizza" placeholder="Coste de la Pizza..." required><br>
 
                 <label for="precioPizza">Precio de la Pizza:</label>
-                <input value="<?php echo htmlspecialchars($precioPizza); ?>" name="precioPizza"
-                    placeholder="Precio de la Pizza..." required><br>
+                <input value="<?php echo htmlspecialchars($precioPizza); ?>" name="precioPizza" placeholder="Precio de la Pizza..." required><br>
 
                 <label for="ingredientesPizza">Ingredientes de la Pizza:</label>
-                <input value="<?php echo htmlspecialchars($ingredientesPizza); ?>" name="ingredientesPizza"
-                    placeholder="Ingredientes de la Pizza..." required><br>
+                <input value="<?php echo htmlspecialchars($ingredientesPizza); ?>" name="ingredientesPizza" placeholder="Ingredientes de la Pizza..." required><br>
 
                 <button type="submit" name="crear">Enviar</button>
             </form>
-
 
             <?php
             listarPizzas($conn);
             ?>
 
             <a href='index.php?logout=true'>Cerrar Sesión</a>
-        </div>
     </div>
-
 </body>
 
 </html>
+
+
