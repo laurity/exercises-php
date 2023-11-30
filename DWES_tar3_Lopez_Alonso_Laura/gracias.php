e7464d245aee0de591f605d3468d4b3004fac6ad
Laura López <99351746+laurity@users.noreply.github.com>
2023-12-01 00:48:30 +0100

    corregida ruta

--- DWES_tar3_Lopez_Alonso_Laura/gracias.php
+++ DWES_tar3_Lopez_Alonso_Laura/gracias.php
@@ -10,7 +10,7 @@
     <h1>¡Gracias por tu pedido!</h1>
     <p>Te agradecemos por elegir nuestros productos/servicios. Tu pedido ha sido recibido y está siendo procesado.</p>
 
-    <a href="pedido.php">Volver a la página de inicio</a>
+    <a href="index.php">Volver a la página de inicio</a>
 
 </body>
 </html>
